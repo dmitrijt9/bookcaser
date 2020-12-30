84350ba76d68ce8c8db4f991e66b3a8a8b46340a
dmitrijt9 <dmitrijt9@users.noreply.github.com>
2020-12-30 11:14:12 +0000

    deploy: 0648425030bbb172eaa5ea6e1ce982539ad76fb8

---
+++ _nuxt/static/1609326850/app/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1609326850",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fapp",config:{axios:{browserBaseURL:"https:\u002F\u002Fwww.googleapis.com\u002Fbooks\u002Fv1\u002F"}}}}(null));
