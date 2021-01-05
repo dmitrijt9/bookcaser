b31535aa08a37240c221557b844635577fe27ba8
dmitrijt9 <dmitrijt9@users.noreply.github.com>
2021-01-05 22:26:08 +0000

    deploy: 73e2dcb4fc1294b263e6cf814b078c747a238e6f

--- _nuxt/static/1609594852/discover/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1609594852",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fdiscover",config:{axios:{browserBaseURL:"https:\u002F\u002Fwww.googleapis.com\u002Fbooks\u002Fv1\u002F"}}}}(null));
